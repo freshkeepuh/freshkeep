0131073e3239e7678d3352592280d6e3dd66c0b7
Joseph Heintz <143227278+josephheintz@users.noreply.github.com>
2025-09-08 13:55:51 -1000

    Created Location skeleton page and added to Navbar

---
+++ src/app/location/page.tsx
@@ -0,0 +1,17 @@
+import { Col, Container, Image, Row } from 'react-bootstrap';
+
+/* Location Management Skeleton Page */
+const LocationManagement = () => (
+  <main>
+    <Container id="location-page" fluid className="py-3">
+      <Row className="align-middle text-center">
+        <Col xs={8} className="d-flex flex-column justify-content-center">
+          <h1>Location *Skeleton* Page</h1>
+          <p>Test</p>
+        </Col>
+      </Row>
+    </Container>
+  </main>
+);
+
+export default LocationManagement;
