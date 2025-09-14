e945f3e1cb57c9b187e0ebda103bbca09cd2997e
awjans <andreaw@jans.org>
2025-09-13 17:21:29 -1000

    Refactor tests: update branch naming convention and clean up test cases

---
+++ tests/user-pages.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from './auth-utils';
+
+test.slow();
+test('test access to user page', async ({ getUserPage }) => {
+  // Call the getUserPage fixture with users signin info to get authenticated session for user
+  const customUserPage = await getUserPage('john@foo.com', 'changeme');
+
+  // Navigate to the home customUserPage
+  await customUserPage.goto('http://localhost:3000/');
+  await expect(customUserPage.getByRole('button', { name: 'john@foo.com' })).toBeVisible();
+});
