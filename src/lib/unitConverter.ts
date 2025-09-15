4203dd9dc632734352b96672fac0dbba26308d32
awjans <andreaw@jans.org>
2025-09-15 10:07:33 -1000

    Enhance Prisma schema with detailed comments and improve seed functions for users, units, grocery items, locations, containers, and items

---
+++ src/lib/unitConverter.ts
@@ -0,0 +1,23 @@
+import { Unit } from '@prisma/client';
+
+/**
+ * Converts the From Unit/Qty to the To Unit.
+ * @param fromUnit The From Unit
+ * @param fromQty The From Quanity
+ * @param toUnit The To Unit
+ * @returns The Quantity in the To Unit
+ */
+export const unitConverter = ({
+    fromUnit,
+    fromQty,
+    toUnit,
+}: {
+    fromUnit: Unit,
+    fromQty: number,
+    toUnit: Unit,
+}): number => {
+    if (fromUnit.id === toUnit.id) {
+        return fromQty;
+    }
+    return fromQty * fromUnit.factor / toUnit.factor;
+};
