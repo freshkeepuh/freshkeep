b31b2db4b634fa592bd4e54681a06a2b97fbd9a4
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2025-09-23 12:38:20 -1000

    created a shop model and added shop button to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/jsx-indent, @typescript-eslint/indent */
+
 'use client';
 
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { BoxArrowRight, Lock } from 'react-bootstrap-icons';
+import { BoxArrowRight, Lock, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
-// TODO: custom colors https://getbootstrap.com/docs/5.3/customize/color-modes/
 const NavBar: React.FC = () => {
   const { data: session } = useSession();
   const currentUser = session?.user?.email;
@@ -13,35 +14,52 @@ const NavBar: React.FC = () => {
   return (
     <Navbar bg="success" expand="lg" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="/" className="d-flex align-items-center">FreshKeep</Navbar.Brand>
-        <Nav>
-          {session && <Navbar.Toggle aria-controls="basic-navbar-nav" />}
-          {session ? (
-            <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Brand href="/">FreshKeep</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto justify-content-center">
+            {currentUser
+              ? [
+                  <Nav.Link
+                    id="add-stuff-nav"
+                    href="/shoppingList"
+                    key="shoppingList"
+                    active={pathName === '/shoppingList'}
+                  >
+                    Shopping List
+                  </Nav.Link>,
+                  <Nav.Link id="list-stuff-nav" href="/shop" key="shop" active={pathName === '/shop'}>
+                    Shop
+                  </Nav.Link>,
+                ]
+              : ''}
+          </Nav>
+          <Nav>
+            {session ? (
               <NavDropdown id="login-dropdown" title={currentUser}>
                 <NavDropdown.Item id="login-dropdown-sign-out" href="/api/auth/signout">
                   <BoxArrowRight />
-                  {' '}
                   Sign Out
                 </NavDropdown.Item>
                 <NavDropdown.Item id="login-dropdown-change-password" href="/auth/change-password">
                   <Lock />
-                  {' '}
                   Change Password
                 </NavDropdown.Item>
               </NavDropdown>
-            </Navbar.Collapse>
-          ) : (
-            <Nav.Link
-              id="login-dropdown-sign-in"
-              title="Login"
-              href="/auth/signin"
-              active={pathName === '/auth/signin'}
-            >
-              Sign In
-            </Nav.Link>
-          )}
-        </Nav>
+            ) : (
+              <NavDropdown id="login-dropdown" title="Login">
+                <NavDropdown.Item id="login-dropdown-sign-in" href="/auth/signin">
+                  <PersonFill />
+                  Sign in
+                </NavDropdown.Item>
+                <NavDropdown.Item id="login-dropdown-sign-up" href="/auth/signup">
+                  <PersonPlusFill />
+                  Sign up
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
