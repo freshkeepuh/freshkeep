94be3cf0be2c2b31c33da06d7142f3a3286c5f8a
DMITRY GORDEEV <gordeev.du@gmail.com>
2025-09-29 22:25:29 -1000

    storage card - beggining

--- src/components/Storage.tsx
+++ src/components/Storage.tsx
@@ -1,8 +1,17 @@
-'use client';
+import Card from 'react-bootstrap/Card';
 
 const Storage = () => (
   <div>
-    Storage Page
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the cards content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
   </div>
 );
 
