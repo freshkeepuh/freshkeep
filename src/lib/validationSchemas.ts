7d86cb4facaaf9e86ecbddc864a6f9a5eef34c1d
Phoebe <pchang6@hawaii.edu>
2025-10-07 20:54:36 -1000

    Update validationSchemas.ts

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { ContainerType } from '@prisma/client';
 import { checkUser } from './dbUserActions';
 
 export const forgotPasswordValidation = Yup.object().shape({
@@ -36,4 +37,43 @@ export const signInValidation = Yup.object().shape({
     .required('Password is required')
     .min(6, 'Password must be at least 6 characters')
     .max(20, 'Password must not exceed 20 characters'),
+}); // Prisma enum for container types
+
+export const AddStorageSchema = Yup.object({
+  location: Yup.string().required(),
+  name: Yup.string().required(),
+  type: Yup.mixed<ContainerType>()
+    .oneOf(Object.values(ContainerType))
+    .required(),
+  picture: Yup.string().url('Must be a valid URL').required(),
+  items: Yup.array().of(Yup.object({
+    picture: Yup.string().url('Must be a valid URL').nullable(),
+    id: Yup.string().required(),
+    locId: Yup.string().required(),
+    conId: Yup.string().required(),
+    grocId: Yup.string().required(),
+    unitId: Yup.string().required(),
+    quantity: Yup.number().positive().required(),
+    expiresAt: Yup.date().nullable(),
+  })),
+});
+
+export const EditStorageSchema = Yup.object({
+  id: Yup.string().required(),
+  locId: Yup.string().required(),
+  name: Yup.string().required(),
+  type: Yup.mixed<ContainerType>()
+    .oneOf(Object.values(ContainerType))
+    .required(),
+  picture: Yup.string().url('Must be a valid URL').nullable(),
+  items: Yup.array().of(Yup.object({
+    picture: Yup.string().url('Must be a valid URL').nullable(),
+    id: Yup.string().required(),
+    locId: Yup.string().required(),
+    conId: Yup.string().required(),
+    grocId: Yup.string().required(),
+    unitId: Yup.string().required(),
+    quantity: Yup.number().positive().required(),
+    expiresAt: Yup.date().nullable(),
+  })),
 });
