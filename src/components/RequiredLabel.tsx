f7df62562fe98110daf454a5648234d3972610b1
awjans <andreaw@jans.org>
2025-11-18 12:34:31 -1000

    Refactor RequiredLabel: add htmlFor prop to improve accessibility

--- src/components/RequiredLabel.tsx
+++ src/components/RequiredLabel.tsx
@@ -1,10 +1,10 @@
 import { Form } from 'react-bootstrap';
 
-export default function RequiredLabel({ children }: { children: React.ReactNode }) {
+export default function RequiredLabel({ children, htmlFor }: { children: React.ReactNode; htmlFor: string }) {
   return (
-    <Form.Label aria-required="true">
+    <Form.Label aria-required="true" htmlFor={htmlFor}>
       {children}
-      {' '}
+      &nbsp;
       <span style={{ color: 'red' }}>*</span>
     </Form.Label>
   );
