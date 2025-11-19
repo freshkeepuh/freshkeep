f9ac58d53a28f1b7635b5a6c62e2123cf480f475
Ellie Ishii <166664143+ellieishii@users.noreply.github.com>
2025-11-18 21:39:10 -1000

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -480,7 +480,7 @@ type SeedRecipe = {
   difficulty: 'EASY' | 'NORMAL' | 'HARD';
   diet: 'ANY' | 'VEGAN' | 'VEGETARIAN' | 'PESCETARIAN';
   ingredients: string[];
-  instructions?: { step: number; text: string }[];
+  instructions?: string[];
   image?: string | null;
 };
 
