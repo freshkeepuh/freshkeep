63026152ddd8e1d047248b67afd194e04ef89d6b
Phoebe <pchang6@hawaii.edu>
2025-11-19 07:04:02 -1000

    Update AddLocationForm.tsx

--- src/components/AddLocationForm.tsx
+++ src/components/AddLocationForm.tsx
@@ -1,8 +1,1 @@
 'use client';
-
-import React from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { AddLocationSchema } from '@/lib/validationSchemas';
-import { Location } from '@prisma/client';
-
