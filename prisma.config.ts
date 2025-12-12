f359d3fed110763fdb61263354dad9d1d991fdce
DMITRY GORDEEV <gordeev.du@gmail.com>
2025-12-12 10:34:06 -1000

    Revert "prisma upgrade"
    This reverts commit 7aa7c7874538e0386e316168b602f35478105966.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
   migrations: {
     seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
-  },
-  datasource: {
-    url: env('DATABASE_URL'),
   },
 });
