7aa7c7874538e0386e316168b602f35478105966
DMITRY GORDEEV <gordeev.du@gmail.com>
2025-12-11 21:06:38 -1000

    prisma upgrade

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'prisma/config';
+import { defineConfig, env } from 'prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
   migrations: {
     seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
   },
 });
