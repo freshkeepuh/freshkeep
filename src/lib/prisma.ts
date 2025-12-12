6c7dd48146dfc1587a3e7db960208389a6f49671
DMITRY GORDEEV <gordeev.du@gmail.com>
2025-12-11 22:25:56 -1000

    prisma update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,7 @@
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const { DATABASE_URL } = process.env;
-
-if (!DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
-}
-
-const pool = new Pool({ connectionString: DATABASE_URL });
-
-const prisma = new PrismaClient({
-  adapter: new PrismaPg(pool),
-});
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
