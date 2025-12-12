3ee0cd9973871bc4899db3d4244c0a2e0214dbe9
DMITRY GORDEEV <gordeev.du@gmail.com>
2025-12-12 10:33:55 -1000

    Revert "prisma update"
    This reverts commit 6c7dd48146dfc1587a3e7db960208389a6f49671.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,17 @@
-import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+import { PrismaClient } from '@prisma/client';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const { DATABASE_URL } = process.env;
+
+if (!DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set');
+}
+
+const pool = new Pool({ connectionString: DATABASE_URL });
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+});
 
 export default prisma;
